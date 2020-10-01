71827f0fb9a3f08c87c6635efb287c1e6cfeded0
AdmajaRois <rois.da.silva@gmail.com>
2020-10-01 12:30:21 +0700

    add template in front  end

---
+++ app/Views/layout/page_layout.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Document</title>
+
+    <!-- Bootstrap Template -->
+    <link rel="stylesheet" href="theme/css/bootstrap.css"/>
+    <link rel="stylesheet" href="theme/vendors/linericon/style.css"/>
+    <link rel="stylesheet" href="theme/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="theme/css/themify-icons.css"/>
+    <link rel="stylesheet" href="theme/css/flaticon.css"/>
+    <link rel="stylesheet" href="theme/vendors/owl-carousel/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="theme/vendors/lightbox/simpleLightbox.css"/>
+    <link rel="stylesheet" href="theme/vendors/nice-select/css/nice-select.css"/>
+    <link rel="stylesheet" href="theme/animate-css/animate.css"/>
+    <link rel="stylesheet" href="theme/vendors/jquery-ui/jquery-ui.css"/>
+
+    <!-- Main Css -->
+    <link rel="stylesheet" href="theme/css/style.css"/>
+    <link rel="stylesheet" href="theme/css/responsive.css"/>
+</head>
+<body>
+    <?= $this->include('layout/navbar'); ?>
+    
+    <?= $this->renderSection('content'); ?>
+
+    <?= $this->include('layout/footer'); ?>
+
+    <script src="theme/js/jquery-3.2.1.min.js"></script>
+    <script src="theme/js/popper.js"></script>
+    <script src="theme/js/bootstrap.min.js"></script>
+    <script src="theme/js/stellar.js"></script>
+    <script src="theme/vendors/lightbox/simpleLightbox.min.js"></script>
+    <script src="theme/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+    <script src="theme/vendors/isotope/imageloaded.pkgd.min.js"></script>
+    <script src="theme/vendors/isotope/isotope-min.js"></script>
+    <script src="theme/vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="theme/js/jquery.ajaxchimp.min.js"></script>
+    <script src="theme/vendors/counter-up/jquery.waypoints.min.js"></script>
+    <script src="theme/vendors/counter-up/jquery.counterup.js"></script>
+    <script src="theme/js/mail-script.js"></script>
+    <script src="theme/js/theme.js"></script>
+</body>
+</html>
